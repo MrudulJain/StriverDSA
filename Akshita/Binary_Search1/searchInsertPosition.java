ce81fc1037272d1c49b4ce2066bdcea92cb2cc05
akshita <akshitasemwal@gmail.com>
2022-12-11 21:15:54 +0530

    Programs based on divide and conqueor approach

---
+++ Akshita/Binary_Search1/searchInsertPosition.java
@@ -0,0 +1,45 @@
+//Given a sorted array Arr[](0-index based) consisting of N distinct integers and an integer k.
+// The task is to find the index of k, if its present in the array Arr[].
+// Otherwise, find the index where k must be inserted to keep the array sorted.
+//TC: O(log n)
+
+package Binary_Search1;
+import java.util.*;
+public class searchInsertPosition {
+    public static void main(String[] args)
+    {
+        int n=4;
+        int[] nums= {1,3,5,6};
+        int target=5;
+        System.out.println(searchInsertK(nums,n, target));  //prints the index of the element
+    }
+
+    public static int searchInsertK(int arr[], int N, int target)
+    {
+        int high=N-1;
+        int low=0;
+        int mid=low+(high-low)/2;
+        if(target>arr[high])
+        {
+            return high+1;
+        }
+        while(high>=low)
+        {
+            mid=low+(high-low)/2;
+            if(target==arr[mid])
+            {
+                return mid;
+            }
+            if(target>arr[mid])
+            {
+                low=mid+1;
+            }
+            if(target<arr[mid])
+            {
+                high=mid-1;
+            }
+        }
+        return low;  //low is returned because we have to find the position at which the element can be inserted
+    }
+
+}
